cd5e5e75136c354e05cf6d49d4dce46bcc8f5428
yolomias <yolomeus420@gmail.com>
2018-05-02 10:52:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        blazeIt();
+    }
+
+    private static void doNothing()
+    {
+
+    }
+
+    private static void blazeIt ()
+    {
+        System.out.println("YOLO WORLD !1!1!11");
+        doNothing();
+    }
+}
